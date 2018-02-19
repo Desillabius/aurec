3ade738e83ebafcd0e25513b502b8f8c019b7e79
Giulio Bramante <giulio.bramante@arcww.leoburnett.it>
2018-02-19 18:27:31 +0100

    test nuova pagina

---
+++ src/theme/page-homepage-en.php
@@ -0,0 +1,155 @@
+<?php get_header(); ?>
+<section>
+	<div class="hero__wrapper animate-item" data-animation="zoom-in-hero">
+		<!-- <div class="hero__image lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/hero/AUREC-precious-metals.jpg" alt="Au.Rec - Metalli Preziosi" 
+			data-start="transform: translateY(0px);"
+			data-500-start="transform: translateY(150px);"
+			data-anchor-target=".hero__wrapper"></div> -->
+		<div class="hero__image lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/hero/AUREC-precious-metals.jpg" alt="Au.Rec - Precious Metals"></div>
+	</div>
+</section>
+<section>
+	<div class="wrapper title__wrapper">
+		<h2 class="gold-title animate-item" data-animation="zoom-in">RECOVER, RECYCLE, TRANSFORM.</h2>
+		<p class="subtitle animate-item" data-animation="zoom-in">AU.REC, in collaboration with BM Valenza, deals with the recovery and refining of precious metals. <br>
+		A project created to connect the professionals of the dental and prosthodontics field with the Valenza city smelting centers, always guaranteeing a precise and transparent evaluation.</p>
+		<div class="anchors__wrapper">
+			<div class="anchor js-anchor animate-item" data-animation="zoom-in-txt" data-anchor="#anchor-1">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/consulenza.svg" alt="Technical consultancy">
+				<p class="anchor__text">Technical consultancy</p>
+			</div>
+			<div class="anchor js-anchor animate-item" data-animation="zoom-in-txt" data-anchor="#anchor-2">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/valutazione.svg" alt="Evaluation of materials and spectrometric screening">
+				<p class="anchor__text">Evaluation of materials and spectrometric screening</p>
+			</div>
+			<div class="anchor js-anchor animate-item" data-animation="zoom-in-txt" data-anchor="#anchor-3">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/recupero.svg" alt="Precious orthodontic metals recovery and tax advice">
+				<p class="anchor__text">Precious orthodontic metals recovery and tax advice</p>
+			</div>
+			<div class="anchor js-anchor animate-item" data-animation="zoom-in-txt" data-anchor="#anchor-4">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/investimenti.svg" alt="Precious metals and diamond investment consulting">
+				<p class="anchor__text">Precious metals and diamond investment consulting</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="wrapper cta__wrapper animate-item" data-animation="zoom-in">
+		<div class="cta__left">
+			<h3 class="text-grey">The perfect solution, tailored to your needs.</h3>
+			<p>
+				With a whole range of services, AU.REC <br>
+				has always a bespoke answer to all your needs. <br>
+				Book now a consultation, it’s free and with no obligation.
+			</p>
+		</div>
+		<div class="cta__right">
+			<a class="cta mobile-hide" href="mailto:info@aurec.it" title="Book now a consultation">Book now <br>a consultation</a>
+			<a class="cta mobile-show" href="tel:+3901311826242" title="Book now a consultation">Book now <br>a consultation</a>
+		</div>
+	</div>
+</section>
+<main>
+	<div id="anchor-1" class="box__wrapper grey-bg">
+		<div class="img__wrapper animate-item" data-animation="zoom-in">
+			<div class="box__img lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/servizi/consulenza-tecnica.jpg" alt="Consulenza tecnica" 
+			data--200-top="transform: translateY(60px);"
+			data-center="transform: translateY(0px);"
+			data-200-bottom="transform: translateY(-60px);"
+			data-anchor-target="#anchor-1"></div>
+		</div>
+		<div class="text__wrapper animate-item" data-animation="zoom-in-txt">
+			<div class="text__inner">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/consulenza.svg" alt="Consulenza tecnica">
+				<h5>TECHNICAL CONSULTANCY</h5>
+				<p>AU.REC offers several evaluation options based on available time and quantity of materials to be analyzed. <br>An expert will evaluate the material directly on site, in your office or laboratory, with just a simple appointment.</p>
+			</div>
+		</div>
+	</div>
+	<div id="anchor-2" class="box__wrapper">
+		<div class="text__wrapper animate-item" data-animation="zoom-in-txt">
+			<div class="text__inner">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/valutazione.svg" alt="Valutazione materiali e screening">
+				<h5>EVALUATION OF MATERIALS <br>AND SPECTROMETRIC SCREENING</h5>
+				<p>The AU.REC experts offer an accurate evaluation to calculate the purity or the carats of precious metals such as silver, gold, platinum and palladium. Our technicians specialize in the listing of medical dental devices as well as the evaluation of the waste in goldsmith, electronic, engineering and mining industry.</p>
+			</div>
+		</div>
+		<div class="img__wrapper animate-item" data-animation="zoom-in">
+			<div class="box__img lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/servizi/valutazioni-materiali.jpg" alt="Consulenza tecnica" 
+			data--200-top="transform: translateY(60px);"
+			data-center="transform: translateY(0px);"
+			data-200-bottom="transform: translateY(-60px);"
+			data-anchor-target="#anchor-2"></div>
+		</div>
+	</div>
+	<div id="anchor-3" class="box__wrapper grey-bg">
+		<div class="img__wrapper animate-item" data-animation="zoom-in">
+			<div class="box__img lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/servizi/recupero-leghe.jpg" alt="Consulenza tecnica" 
+			data--200-top="transform: translateY(60px);"
+			data-center="transform: translateY(0px);"
+			data-200-bottom="transform: translateY(-60px);"
+			data-anchor-target="#anchor-3"></div>
+		</div>
+		<div class="text__wrapper animate-item" data-animation="zoom-in-txt">
+			<div class="text__inner">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/recupero.svg" alt="Recupero leghe e consulenza fiscale">
+				<h5>PRECIOUS ORTHODONTIC METALS RECOVERY AND TAX ADVICE</h5>
+				<p>The AU.REC consultant offers an accurate evaluation and quantification of metals recyclable through smelting and identifies a personalized recovery plan.</p>
+			</div>
+		</div>
+	</div>
+	<div id="anchor-4" class="box__wrapper">
+		<div class="text__wrapper animate-item" data-animation="zoom-in-txt">
+			<div class="text__inner">
+				<svg data-src="<?php echo get_template_directory_uri(); ?>/img/icons/investimenti.svg" alt="Consulenza su investimenti in metallo e diamanti">
+				<h5>PRECIOUS METALS <br>AND DIAMOND INVESTMENT CONSULTING</h5>
+				<p>The AU.REC range of services deals with providing specific technical consultancy and tax advice for all those interested in investing in precious metals and diamonds.</p>
+			</div>
+		</div>
+		<div class="img__wrapper animate-item" data-animation="zoom-in">
+			<div class="box__img lazy" data-src="<?php echo get_template_directory_uri(); ?>/img/servizi/consulenza-investimenti.jpg" alt="Consulenza tecnica" 
+			data--200-top="transform: translateY(60px);"
+			data-center="transform: translateY(0px);"
+			data-200-bottom="transform: translateY(-60px);"
+			data-anchor-target="#anchor-4"></div>
+		</div>
+	</div>
+</main>
+<section class="grey-bg">
+	<div class="wrapper title__wrapper animate-item" data-animation="zoom-in">
+		<svg class="quotazioni-svg" data-src="<?php echo get_template_directory_uri(); ?>/img/icons/quotazioni.svg" alt="Quotazioni">
+		<h3 class="title-2">QUOTATIONS</h3>
+		<h4>Find the market rate of precious metals in real time.</h4>
+	</div>
+</section>
+<section>
+	<!-- <div class="wrapper animate-item" data-animation="zoom-in">
+		<p class="trading-desc">Prezzi per oncia in Euro</p>
+		<div id="trading-view-it"></div>
+	</div> -->
+	<div class="wrapper wrapper--chart animate-item" data-animation="zoom-in">
+		<script type="text/javascript" src="//www.bullionvault.com/chart/bullionvaultchart.js" ></script>
+		<script type="text/javascript" >
+			var options = {
+				bullion: 'gold',
+				currency: 'EUR',
+				timeframe: '1m',
+				chartType: 'line',
+				miniChartModeAxis : 'both',
+				containerDefinedSize: true,
+				miniChartMode: false,
+				displayLatestPriceLine: false,
+				switchBullion: true,
+				switchCurrency: true,
+				switchTimeframe: true,
+				switchChartType: false,
+				exportButton: false
+			};
+			var chartBV = new BullionVaultChart(options, 'bullionvaultchart-EN');
+		</script>
+		<!-- <p class="trading-desc">Prezzo al kilo e all'oncia in Euro</p> -->
+		<div class="bullionvaultchart" id="bullionvaultchart-EN"></div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
